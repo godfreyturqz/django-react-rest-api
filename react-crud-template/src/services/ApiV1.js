8959523b57b25594e6ffdcaf6efa6200d6aca212
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-04-18 19:45:18 +0800

    update Api

--- react-crud-template/src/services/ApiV1.js
+++ react-crud-template/src/services/ApiV1.js
@@ -5,7 +5,7 @@ const API_BASE_URL = 'http://127.0.0.1:8000/api/v1'
 export const api = (httpReqMethod, id = '', objectData = {}) => {
 
     const config = {
-        url: `${API_BASE_URL}/${id}`,
+        url: `${API_BASE_URL}/register/${id}`,
         method: httpReqMethod,
         data: objectData
     }
---
+++ react-crud-template/src/services/ApiV2.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+export class ApiRequest {
+    constructor(){
+        this.API_BASE_URL = 'http://127.0.0.1:8000/api/v1'
+    }
+
+    register(httpReqMethod, id = '', objectData = {}){
+        return axios({
+            url: `${this.API_BASE_URL}/register/${id}`,
+            method: httpReqMethod,
+            data: objectData
+        })
+    }
+
+    rfi(httpReqMethod, id = '', objectData = {}){
+        return axios({
+            url: `${this.API_BASE_URL}/rfi/${id}`,
+            method: httpReqMethod,
+            data: objectData
+        })
+    }
+}
