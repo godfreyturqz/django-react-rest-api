660f447a60030d887dbc7128afed237d1361717d
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-04-20 20:18:18 +0800

    added link for different apps

--- react-crud-template/src/App.js
+++ react-crud-template/src/App.js
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 const Register = lazy(() => import('./pages/Register'))
 const Rfi = lazy(() => import('./pages/Rfi'))
 
@@ -11,7 +11,12 @@ function App() {
     <div className="container">
       <h1>CRUD app for testing REST API</h1>
       <br/>
+      
       <BrowserRouter>
+      <div className="d-flex ">
+        <Link to='/register'>Register</Link>
+        <Link to='/rfi'>RFI</Link>
+      </div>
       <Suspense fallback={<Loading/>}>
         <Switch>
           <Route path='/register' component={Register}/>
